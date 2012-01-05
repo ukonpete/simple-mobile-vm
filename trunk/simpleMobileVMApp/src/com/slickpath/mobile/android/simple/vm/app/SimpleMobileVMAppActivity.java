d38d8751567aa0f3dd516d0dfce2c21a847f4416
ukonpete@gmail.com <ukonpete@gmail.com@661b4331-e04f-a31d-c30e-921faf56ea9a>
2012-01-05 07:17:48 +0000

    Fibonacci AGAIN

--- trunk/simpleMobileVMApp/src/com/slickpath/mobile/android/simple/vm/app/SimpleMobileVMAppActivity.java
+++ trunk/simpleMobileVMApp/src/com/slickpath/mobile/android/simple/vm/app/SimpleMobileVMAppActivity.java
@@ -5,26 +5,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.slickpath.mobile.android.simple.vm.*;
-import com.slickpath.mobile.android.simple.vm.app.R;
-import com.slickpath.mobile.android.simple.vm.app.R.id;
-import com.slickpath.mobile.android.simple.vm.app.R.layout;
+import com.slickpath.mobile.android.simple.vm.SimpleParser;
+import com.slickpath.mobile.android.simple.vm.VMError;
+import com.slickpath.mobile.android.simple.vm.VirtualMachine;
+import com.slickpath.mobile.android.simple.vm.VMListener;
 
-public class SimpleMobileVMAppActivity extends Activity {
+public class SimpleMobileVMAppActivity extends Activity implements VMListener{
 
+	ProgressDialog _dialog;
+	VirtualMachine _vm;
+	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    	
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
         File filesDir = this.getApplicationContext().getFilesDir();
@@ -35,65 +41,98 @@ public class SimpleMobileVMAppActivity extends Activity {
         String[] sFiles = this.getApplicationContext().fileList();
         
         final Spinner spinner = (Spinner) findViewById(R.id.spinnerFiles);
-        {
-        	
-        	ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, sFiles);
-        	spinner.setAdapter(adapter);
-        	/*
-        	ArrayAdapter adapter = new ArrayAdapter(this,
-        			android.R.layout.spinnerFiles, spinner);
-        			s.setAdapter(adapter);
-        			*/
-        }
         
-        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, sFiles);
+        spinner.setAdapter(adapter);
 
-			public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener(){
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) {
 				// TODO Auto-generated method stub
-				parseFile(position);
+				getSelectedFileName(arg2);
 			}
 
+			@Override
 			public void onNothingSelected(AdapterView<?> arg0) {
 				// TODO Auto-generated method stub
 				
+			}});
+        
+        
+        Button exeButton = (Button)findViewById(R.id.buttonExe);
+        exeButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+		        _vm = new VirtualMachine(SimpleMobileVMAppActivity.this.getApplicationContext());
+		        _vm.setVMListener(SimpleMobileVMAppActivity.this);
+				parseFile((int) spinner.getSelectedItemId());
 			}
         });
+        
     }
     
     public void parseFile(int file)
     {
-    	String[] sFiles = this.getApplicationContext().fileList();
-    	
-        TextView textView = (TextView)findViewById(R.id.textViewFile);
-        textView.setText(sFiles[file]);	
-        
-        VirtualMachine vm = new VirtualMachine(this.getApplicationContext());
+    	String sSelectedFile = getSelectedFileName(file);
         
         File filesDir = this.getApplicationContext().getFilesDir();
     
-        SimpleParser parser = new SimpleParser(filesDir.getPath() + File.separator + sFiles[file]);
+        SimpleParser parser = new SimpleParser(filesDir.getPath() + File.separator + sSelectedFile);
+        parser.setVMListener(this);
         
-        List<List<Integer>> allParameters = new ArrayList<List<Integer>>();
-        List<Integer> allInstructions = new ArrayList<Integer>();
-        try {
-        	allInstructions = parser.parse(allParameters);
-		} catch (VMError e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        _dialog = ProgressDialog.show(SimpleMobileVMAppActivity.this, "",
+        		"Please wait for few seconds...", true);
+
+        parser.parse();
+
         
-        try {
-			vm.addInstructions(allInstructions, allParameters);
-		} catch (VMError e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
-        try {
-			vm.runInstructions();
-		} catch (VMError e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
     }
+
+	/**
+	 * @param file
+	 * @return
+	 */
+	private String getSelectedFileName(int file) {
+		String sSelectedFile = "N/A";
+    	String[] sFiles = this.getApplicationContext().fileList();
+    	sSelectedFile = sFiles[file];
+        TextView textView = (TextView)findViewById(R.id.textViewFile);
+        textView.setText(sFiles[file]);
+		return sSelectedFile;
+	}
+
+	@Override
+	public void completedParse(VMError vmError, List<Integer> allInstructions, List<List<Integer>> allParameters)
+	{
+		if ( vmError != null)
+		{
+			System.out.println("ERROR PARSE");
+			vmError.printStackTrace();
+		}
+        _vm.addInstructions(allInstructions, allParameters);
+	}
+
+	@Override
+	public void completedAddingInstructions(VMError vmError) {
+		if ( vmError != null)
+		{
+			System.out.println("ERROR ADD INST");
+			vmError.printStackTrace();
+		}
+		_vm.runInstructions();
+	}
+
+	@Override
+	public void completedRunningInstructions(VMError vmError) {
+		if ( vmError != null)
+		{
+			System.out.println("ERROR RUN INST");
+			vmError.printStackTrace();
+		}
+		_dialog.dismiss();
+	}
 }
