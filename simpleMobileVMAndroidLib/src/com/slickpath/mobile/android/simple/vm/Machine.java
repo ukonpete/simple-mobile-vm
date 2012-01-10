d31eccde93635e7d12dbf866ce8c7f0a301c08d4
ukonpete@gmail.com <ukonpete@gmail.com@661b4331-e04f-a31d-c30e-921faf56ea9a>
2012-01-10 16:05:21 +0000

    More cleanup

--- simpleMobileVMAndroidLib/src/com/slickpath/mobile/android/simple/vm/Machine.java
+++ simpleMobileVMAndroidLib/src/com/slickpath/mobile/android/simple/vm/Machine.java
@@ -17,323 +17,324 @@ import android.util.Log;
 public class Machine extends Kernel{
 
 	private static final String TAG = Machine.class.getName();
-	
+
 	protected final PrintStream _textWriter;
-    protected final Scanner _textScanner;
-    private final InputStream _textReader;
-    
-    protected boolean _bDebug = false;
+	protected final Scanner _textScanner;
+	private final InputStream _textReader;
+
+	protected boolean _bDebug = false;
 	/**
 	 * 
 	 */
 	public Machine(final PrintStream writer, final InputStream reader) {
 		super();
-    	_textWriter = writer;
-    	_textReader = reader;
-    	_textScanner = new Scanner(_textReader);
+		_textWriter = writer;
+		_textReader = reader;
+		_textScanner = new Scanner(_textReader);
 	}
 
-    public Machine()
-    {
-    	super();
-    	_textWriter = System.out;
-    	_textReader = System.in;
-    	_textScanner = new Scanner(_textReader);
-    }
+	public Machine()
+	{
+		super();
+		_textWriter = System.out;
+		_textReader = System.in;
+		_textScanner = new Scanner(_textReader);
+	}
 
-    //  Command Category : ARITHMATIC
+	//  Command Category : ARITHMATIC
 
-    public void ADD() throws VMError
-    {
-        final int val1 = _pop();
-        final int val2 = _pop();
-        final int val3 = val1 + val2;
-        PUSHC(val3);
-    }
+	public void ADD() throws VMError
+	{
+		final int val1 = _pop();
+		final int val2 = _pop();
+		final int val3 = val1 + val2;
+		PUSHC(val3);
+	}
 
-    public void SUB() throws VMError
-    {
-    	final int val1 = _pop();
-    	final int val2 = _pop();
-    	final int val3 = val1 - val2;
-        PUSHC(val3);
-    }
+	public void SUB() throws VMError
+	{
+		final int val1 = _pop();
+		final int val2 = _pop();
+		final int val3 = val1 - val2;
+		PUSHC(val3);
+	}
 
-    public void MUL() throws VMError
-    {
-    	final int val1 = _pop();
-    	final int val2 = _pop();
-    	final int val3 = val1 * val2;
-        PUSHC(val3);
-    }
+	public void MUL() throws VMError
+	{
+		final int val1 = _pop();
+		final int val2 = _pop();
+		final int val3 = val1 * val2;
+		PUSHC(val3);
+	}
 
-    public void DIV() throws VMError
-    {
-    	final int val1 = _pop();
-    	final int val2 = _pop();
-    	final int val3 = val1 / val2;
-        PUSHC(val3);
-    }
+	public void DIV() throws VMError
+	{
+		final int val1 = _pop();
+		final int val2 = _pop();
+		final int val3 = val1 / val2;
+		PUSHC(val3);
+	}
 
-    public void NEG() throws VMError
-    {
-    	final int val1 = _pop();
-    	final int val2 = 0 - val1;
-        PUSHC(val2);
-    }
+	public void NEG() throws VMError
+	{
+		final int val1 = _pop();
+		final int val2 = 0 - val1;
+		PUSHC(val2);
+	}
 
-    //  Command Category : BOOLEAN
+	//  Command Category : BOOLEAN
 
-    public void EQUAL() throws VMError
-    {
-        int equal = YES;
-        final int val1 = _pop();
-        final int val2 = _pop();
+	public void EQUAL() throws VMError
+	{
+		int equal = YES;
+		final int val1 = _pop();
+		final int val2 = _pop();
 
-        if (val1 != val2)
-        {
-            equal = NO;
-        }
-        PUSHC(equal);
-    }
+		if (val1 != val2)
+		{
+			equal = NO;
+		}
+		PUSHC(equal);
+	}
 
-    public void NOTEQL() throws VMError
-    {
-        int notEqual = NO;
-        final int val1 = _pop();
-        final int val2 = _pop();
+	public void NOTEQL() throws VMError
+	{
+		int notEqual = NO;
+		final int val1 = _pop();
+		final int val2 = _pop();
 
-        if (val1 != val2)
-        {
-            notEqual = YES;
-        }
-        PUSHC(notEqual);
-    }
+		if (val1 != val2)
+		{
+			notEqual = YES;
+		}
+		PUSHC(notEqual);
+	}
 
-    public void GREATER() throws VMError
-    {
-        int greater = NO;
-        final int val1 = _pop();
-        final int val2 = _pop();
+	public void GREATER() throws VMError
+	{
+		int greater = NO;
+		final int val1 = _pop();
+		final int val2 = _pop();
 
-        if (val1 > val2)
-        {
-            greater = YES;
-        }
-        PUSHC(greater);
-    }
+		if (val1 > val2)
+		{
+			greater = YES;
+		}
+		PUSHC(greater);
+	}
 
-    public void LESS() throws VMError
-    {
-        int less = NO;
-        final int val1 = _pop();
-        final int val2 = _pop();
+	public void LESS() throws VMError
+	{
+		int less = NO;
+		final int val1 = _pop();
+		final int val2 = _pop();
 
-        if (val1 < val2)
-        {
-            less = YES;
-        }
-        PUSHC(less);
-    }
+		if (val1 < val2)
+		{
+			less = YES;
+		}
+		PUSHC(less);
+	}
 
-    public void GTREQL() throws VMError
-    {
-        int greaterOrEqual = NO;
-        final int val1 = _pop();
-        final int val2 = _pop();
+	public void GTREQL() throws VMError
+	{
+		int greaterOrEqual = NO;
+		final int val1 = _pop();
+		final int val2 = _pop();
 
-        if (val1 >= val2)
-        {
-            greaterOrEqual = YES;
-        }
-        PUSHC(greaterOrEqual);
-    }
+		if (val1 >= val2)
+		{
+			greaterOrEqual = YES;
+		}
+		PUSHC(greaterOrEqual);
+	}
 
-    public void LSSEQL() throws VMError
-    {
-        int lessEqual = NO;
-        final int val1 = _pop();
-        final int val2 = _pop();
+	public void LSSEQL() throws VMError
+	{
+		int lessEqual = NO;
+		final int val1 = _pop();
+		final int val2 = _pop();
 
-        if (val1 <= val2)
-        {
-            lessEqual = YES;
-        }
-        PUSHC(lessEqual);
-    }
+		if (val1 <= val2)
+		{
+			lessEqual = YES;
+		}
+		PUSHC(lessEqual);
+	}
 
-    public void NOT() throws VMError
-    {
-        int val = _pop();
+	public void NOT() throws VMError
+	{
+		int val = _pop();
 
-        if (val == 0)
-        {
-            val = 1;
-        }
-        else
-        {
-            val = 0;
-        }
+		if (val == 0)
+		{
+			val = 1;
+		}
+		else
+		{
+			val = 0;
+		}
 
-        PUSHC(val);
-    }
+		PUSHC(val);
+	}
 
-    //  Command Category : STACK_MANIPULATION
+	//  Command Category : STACK_MANIPULATION
 
-    public void PUSH(final int location ) throws VMError
-    {
-    	final int val = getValueAt(location);
-        PUSHC(val);
-    }
+	public void PUSH(final int location ) throws VMError
+	{
+		final int val = getValueAt(location);
+		PUSHC(val);
+	}
 
-    public void PUSHC(final int value) throws VMError
-    {
-        if (isStackPointerValid())
-        {
-           incStackPtr();
-           setValAtLocation(value, getStackPointer());
-        }
-        else
-        {
-        	throw new VMError("PUSHC", VMError.VM_ERROR_TYPE_STACK_LIMIT);
-        }
-    }
+	public void PUSHC(final int value) throws VMError
+	{
+		if (isStackPointerValid())
+		{
+			incStackPtr();
+			setValAtLocation(value, getStackPointer());
+		}
+		else
+		{
+			throw new VMError("PUSHC", VMError.VM_ERROR_TYPE_STACK_LIMIT);
+		}
+	}
 
-    public void POP() throws VMError
-    {
-    	final int location = _pop();
-    	final int value = _pop();
-        setValAtLocation(value, location);
-    }
+	public void POP() throws VMError
+	{
+		final int location = _pop();
+		final int value = _pop();
+		setValAtLocation(value, location);
+	}
 
-    public void POPC(final int location) throws VMError
-    {
-    	final int value = _pop();
-        setValAtLocation(value, location);
-    }
+	public void POPC(final int location) throws VMError
+	{
+		final int value = _pop();
+		setValAtLocation(value, location);
+	}
 
-    //  Command Category : INPUT/OUTPUT
+	//  Command Category : INPUT/OUTPUT
 
-    public void RDCHAR() throws VMError
-    {
+	public void RDCHAR() throws VMError
+	{
 		try {
 			final char ch = (char)_textReader.read();
-			final int iAsciiValue = (int)ch;
-	        PUSHC(iAsciiValue);
-		} catch (IOException e) {
+			final int iAsciiValue = ch;
+			PUSHC(iAsciiValue);
+		} catch (final IOException e) {
 			throw new VMError("RDCHAR", e, VMError.VM_ERROR_TYPE_IO);
 		}
-    }
+	}
 
-    public void WRCHAR() throws VMError
-    {
-    	final int value = _pop();
-    	final String sVal = Integer.toString(value);
-        _textWriter.print(sVal);
-        debug("WRCHAR: " + sVal);
-    }
+	public void WRCHAR() throws VMError
+	{
+		final int value = _pop();
+		final String sVal = Integer.toString(value);
+		_textWriter.print(sVal);
+		debug("WRCHAR: " + sVal);
+	}
 
-    public void RDINT()  throws VMError
-    {
-    	final int val = _textScanner.nextInt();
-        PUSHC(val);
-    }
+	public void RDINT()  throws VMError
+	{
+		final int val = _textScanner.nextInt();
+		PUSHC(val);
+	}
 
-    public void WRINT()  throws VMError
-    {
-    	final int value = _pop();
-        final String sOut = Integer.toString(value);
-        _textWriter.println(sOut);
-        debug("WRINT" + sOut);
-    }
+	public void WRINT()  throws VMError
+	{
+		final int value = _pop();
+		final String sOut = Integer.toString(value);
+		_textWriter.println(sOut);
+		debug("WRINT" + sOut);
+	}
 
-    //  Command Category : CONTROL
+	//  Command Category : CONTROL
 
-    public void BRANCH(final int location) throws VMError
-    {
-        debug("--BR=" + location);
-        _branch(location);
-        debug("--BR=" + getProgramCounter());
-    }
+	public void BRANCH(final int location) throws VMError
+	{
+		debug("--BR=" + location);
+		debug("--BR=" + getProgramCounter());
+		_branch(location);
+		debug("--BR=" + getProgramCounter());
+	}
 
-    public void JUMP() throws VMError
-    {
-        _jump();
-    }
+	public void JUMP() throws VMError
+	{
+		_jump();
+	}
 
-    public boolean BREQL(final int location) throws VMError
-    {
-    	final int val = _pop();
-    	boolean bBranched = false;
-        if (val == 0)
-        {
-            BRANCH(location);
-            bBranched = true;
-        }
-        return bBranched;
-    }
+	public boolean BREQL(final int location) throws VMError
+	{
+		final int val = _pop();
+		boolean bBranched = false;
+		if (val == 0)
+		{
+			BRANCH(location);
+			bBranched = true;
+		}
+		return bBranched;
+	}
 
-    public boolean BRLSS(final int location) throws VMError
-    {
-    	final int val = _pop();
-    	boolean bBranched = false;
-        if (val < 0)
-        {
-            BRANCH(location);
-            bBranched = true;
-        }
-        return bBranched;
-    }
+	public boolean BRLSS(final int location) throws VMError
+	{
+		final int val = _pop();
+		boolean bBranched = false;
+		if (val < 0)
+		{
+			BRANCH(location);
+			bBranched = true;
+		}
+		return bBranched;
+	}
 
-    public boolean BRGTR(final int location) throws VMError
-    {
-    	final int val = _pop();
-    	boolean bBranched = false;
-        if (val > 0)
-        {
-            BRANCH(location);
-            bBranched = true;
-        }
-        return bBranched;
-    }
+	public boolean BRGTR(final int location) throws VMError
+	{
+		final int val = _pop();
+		boolean bBranched = false;
+		if (val > 0)
+		{
+			BRANCH(location);
+			bBranched = true;
+		}
+		return bBranched;
+	}
 
-    //  Command Category : MISC
+	//  Command Category : MISC
 
-    public void CONTENTS() throws VMError
-    {
-    	final int location = _pop();
-    	final int val = getValueAt(location);
-        PUSHC(val);
-    }
+	public void CONTENTS() throws VMError
+	{
+		final int location = _pop();
+		final int val = getValueAt(location);
+		PUSHC(val);
+	}
 
-    public void HALT()
-    {
-    	// TODO
-    }
+	public void HALT()
+	{
+		debug(TAG, "HALT");
+	}
 
-    protected void setValAtLocation(final int value, final int location) throws VMError
-    {
-        setValueAt(value, location);
-    }
+	protected void setValAtLocation(final int value, final int location) throws VMError
+	{
+		setValueAt(value, location);
+	}
 
-    protected void debug(final String sText)
-    {
-        if ( _bDebug )
-        {
-        	debug(TAG, sText);
-        }
-    }
+	protected void debug(final String sText)
+	{
+		if ( _bDebug )
+		{
+			debug(TAG, sText);
+		}
+	}
 
-    protected void debug(final String sTag, final String sText)
-    {
-        if ( _bDebug )
-        {
-        	Log.d(sTag, sText);
-        }
-    }
+	protected void debug(final String sTag, final String sText)
+	{
+		if ( _bDebug )
+		{
+			Log.d(sTag, sText);
+		}
+	}
 
-    public void setDebug(final boolean bDebug)
-    {
-    	_bDebug = bDebug;
-    }
+	public void setDebug(final boolean bDebug)
+	{
+		_bDebug = bDebug;
+	}
 }
--- simpleMobileVMAndroidLib/src/com/slickpath/mobile/android/simple/vm/parser/SimpleParser.java
+++ simpleMobileVMAndroidLib/src/com/slickpath/mobile/android/simple/vm/parser/SimpleParser.java
@@ -12,13 +12,11 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 
+import android.util.Log;
+
 import com.slickpath.mobile.android.simple.vm.VMError;
 import com.slickpath.mobile.android.simple.vm.instructions.BaseInstructionSet;
 import com.slickpath.mobile.android.simple.vm.util.CommandSet;
-
-import android.util.Log;
-
-
 
 /**
  * @author PJ
@@ -29,40 +27,40 @@ public class SimpleParser {
 	private static final String TAG = SimpleParser.class.getName();
 
 	private ParserListener _parserListener;
- 
-    private String _sInstructionFile;
-    private Hashtable<String, Integer> _htSymbols = new Hashtable<String, Integer>();
-    private Hashtable<String, Integer> _htAddresses = new Hashtable<String, Integer>();
-    private final CommandSet _commandSet = new CommandSet();
-    private int _freeMemoryLoc = 0;
-    
-    protected boolean _bDebug = false;
 
-    public SimpleParser(String sFile)
-    {
-        _sInstructionFile = sFile;
+	private final String _sInstructionFile;
+	private final Hashtable<String, Integer> _htSymbols = new Hashtable<String, Integer>();
+	private final Hashtable<String, Integer> _htAddresses = new Hashtable<String, Integer>();
+	private final CommandSet _commandSet = new CommandSet();
+	private int _freeMemoryLoc = 0;
+
+	protected boolean _bDebug = false;
+
+	public SimpleParser(final String sFile)
+	{
+		_sInstructionFile = sFile;
 		try {
 			Class.forName("BaseInstructionSet");
-		} catch (ClassNotFoundException e) {
+		} catch (final ClassNotFoundException e) {
 			debug(e.getMessage());
 		}
-    }
+	}
 
-    public void setListener(ParserListener listener)
-    {
-    	_parserListener = listener;
-    }
-    
-    public void parse()
-    {
-    	new Thread(new Runnable()
-    	{
+	public void setListener(final ParserListener listener)
+	{
+		_parserListener = listener;
+	}
+
+	public void parse()
+	{
+		new Thread(new Runnable()
+		{
 			public void run()
 			{
 				VMError vmError = null;
 				try {
 					_parse();
-				} catch (VMError e) {
+				} catch (final VMError e) {
 					vmError = e;
 				}
 				if ( _parserListener != null)
@@ -70,164 +68,164 @@ public class SimpleParser {
 					_parserListener.completedParse(vmError, _commandSet);
 				}
 			}
-    	}).start();
-    }
-    
-    public CommandSet getCommandSet()
-    {
-    	return _commandSet;
-    }
-    
-    private void _parse() throws VMError
-    {
-        FileInputStream fis = null;
+		}).start();
+	}
 
-        try
-        {
-            fis = new FileInputStream(_sInstructionFile);
-            getSymbols(fis);
-            fis.close();
-            fis = new FileInputStream(_sInstructionFile);
+	public CommandSet getCommandSet()
+	{
+		return _commandSet;
+	}
 
-            DataInputStream in = new DataInputStream(fis);
-            BufferedReader br = new BufferedReader(new InputStreamReader(in), 8192 );
-            
-            String sLine = br.readLine();
+	private void _parse() throws VMError
+	{
+		FileInputStream fis = null;
 
-            while (sLine != null)
-            {
-                debug("LINE : " + sLine);
-            	
-            	// If line is not a comment
-                if (!sLine.startsWith("//"))
-                {
-                	debug("-Line " + sLine);
-                    String[] words = sLine.split(" ");
+		try
+		{
+			fis = new FileInputStream(_sInstructionFile);
+			getSymbols(fis);
+			fis.close();
+			fis = new FileInputStream(_sInstructionFile);
 
-                    String sInstruction = words[0];
+			final DataInputStream in = new DataInputStream(fis);
+			final BufferedReader br = new BufferedReader(new InputStreamReader(in), 8192 );
 
-                    debug("-RAW " + sInstruction);
-                    // If the line does not start with a symbol
-                    if (!(sInstruction.startsWith("[") && sInstruction.contains("]")))
-                    {
-                        int commandVal = (int)BaseInstructionSet.INSTRUCTION_SET_HT.get(sInstruction);
-                        debug("INST : " + BaseInstructionSet.INSTRUCTION_SET_CONV_HT.get(commandVal) + "(" + commandVal + ")");
-                        List<Integer> parameters = new ArrayList<Integer>();
-                        if (words.length > 1)
-                        {
-                            String sParams = words[1];
-                            String[] InstrParams = sParams.split(",");
-                            int paramVal = -1;
+			String sLine = br.readLine();
 
-                            for (String sParamTemp : InstrParams)
-                            {
-                                String sParam = sParamTemp.trim();
+			while (sLine != null)
+			{
+				debug("LINE : " + sLine);
 
-                                // We found a symbol parameter, replace with line number from symbol table
-                                if (sParam.startsWith("[") && sParam.contains("]"))
-                                {
-                                    int locEnd = sParam.indexOf("]");
-                                    String sSymbol = sParam.substring(1, locEnd);
+				// If line is not a comment
+				if (!sLine.startsWith("//"))
+				{
+					debug("-Line " + sLine);
+					final String[] words = sLine.split(" ");
 
-                                    paramVal = _htSymbols.get(sSymbol);
-                                    debug("  SYMBOL : " + sSymbol + "(" + paramVal + ")");
-                                }
-                                else if (sParam.startsWith("g"))
-                                {
-                                    int memLoc = _freeMemoryLoc;
-                                    if (_htAddresses.containsKey(sParam))
-                                    {
-                                        memLoc = (Integer)_htAddresses.get(sParam);
-                                    }
-                                    else
-                                    {
-                                        _htAddresses.put(sParam, memLoc);
-                                        _freeMemoryLoc++;
-                                    }
-                                    paramVal = memLoc;
-                                    debug("  G-PARAM : " + sParam + "(" + paramVal + ")");
-                                }
-                                else
-                                {
-                                    paramVal = Integer.parseInt(sParam);
-                                    debug("  PARAM : " + sParam);
-                                }
-                                parameters.add(paramVal);
-                            }
-                        }
-                        _commandSet.addCommand(commandVal, parameters);
-                    }
-                }
-                sLine = br.readLine();
-            }
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        	throw new VMError("[runInstructions]" + e.getMessage(), e, VMError.VM_ERROR_TYPE_UNKOWN);
-        }
-        finally
-        {
-            if (fis != null )
-            {
-            	try {
+					final String sInstruction = words[0];
+
+					debug("-RAW " + sInstruction);
+					// If the line does not start with a symbol
+					if (!(sInstruction.startsWith("[") && sInstruction.contains("]")))
+					{
+						final int commandVal = BaseInstructionSet.INSTRUCTION_SET_HT.get(sInstruction);
+						debug("INST : " + BaseInstructionSet.INSTRUCTION_SET_CONV_HT.get(commandVal) + "(" + commandVal + ")");
+						final List<Integer> parameters = new ArrayList<Integer>();
+						if (words.length > 1)
+						{
+							final String sParams = words[1];
+							final String[] InstrParams = sParams.split(",");
+							int paramVal = -1;
+
+							for (final String sParamTemp : InstrParams)
+							{
+								final String sParam = sParamTemp.trim();
+
+								// We found a symbol parameter, replace with line number from symbol table
+								if (sParam.startsWith("[") && sParam.contains("]"))
+								{
+									final int locEnd = sParam.indexOf("]");
+									final String sSymbol = sParam.substring(1, locEnd);
+
+									paramVal = _htSymbols.get(sSymbol);
+									debug("  SYMBOL : " + sSymbol + "(" + paramVal + ")");
+								}
+								else if (sParam.startsWith("g"))
+								{
+									int memLoc = _freeMemoryLoc;
+									if (_htAddresses.containsKey(sParam))
+									{
+										memLoc = _htAddresses.get(sParam);
+									}
+									else
+									{
+										_htAddresses.put(sParam, memLoc);
+										_freeMemoryLoc++;
+									}
+									paramVal = memLoc;
+									debug("  G-PARAM : " + sParam + "(" + paramVal + ")");
+								}
+								else
+								{
+									paramVal = Integer.parseInt(sParam);
+									debug("  PARAM : " + sParam);
+								}
+								parameters.add(paramVal);
+							}
+						}
+						_commandSet.addCommand(commandVal, parameters);
+					}
+				}
+				sLine = br.readLine();
+			}
+		}
+		catch(final Exception e)
+		{
+			e.printStackTrace();
+			throw new VMError("[runInstructions]" + e.getMessage(), e, VMError.VM_ERROR_TYPE_UNKOWN);
+		}
+		finally
+		{
+			if (fis != null )
+			{
+				try {
 					fis.close();
-				} catch (IOException e) {
+				} catch (final IOException e) {
 					throw new VMError("[runInstructions] finally " + e.getMessage(), e, VMError.VM_ERROR_TYPE_IO);
 				}
-            }
-        }
-    }
+			}
+		}
+	}
 
-    /**
-     * Look for lines with symbol definitions (These are lines with "[XYZ]" where XYZ is a String).
-     * If its found save the XYZ String and the line number for later use.
-     * If when parsing a command during parse, we see a symbol (Parameter with "[XYZ]" where XYZ is a String)
-     * we replace that symbol with the line number associated with the matching symbol in the symbol table.
-     */
-    public void getSymbols(FileInputStream fis)
-    {
-        int line = 0;
-        
-        DataInputStream in = new DataInputStream(fis);
-        BufferedReader br = new BufferedReader(new InputStreamReader(in), 8192 );
-        
-        String sLine = "";
-        String sSymbol = "";
+	/**
+	 * Look for lines with symbol definitions (These are lines with "[XYZ]" where XYZ is a String).
+	 * If its found save the XYZ String and the line number for later use.
+	 * If when parsing a command during parse, we see a symbol (Parameter with "[XYZ]" where XYZ is a String)
+	 * we replace that symbol with the line number associated with the matching symbol in the symbol table.
+	 */
+	public void getSymbols(final FileInputStream fis)
+	{
+		int line = 0;
+
+		final DataInputStream in = new DataInputStream(fis);
+		final BufferedReader br = new BufferedReader(new InputStreamReader(in), 8192 );
+
+		String sLine = "";
+		String sSymbol = "";
 		try {
 			sLine = br.readLine();
-            while (sLine != null)
-            {
-                // If line is not a comment
-                if (!sLine.startsWith("//"))
-                {
-                    if (sLine.startsWith("[") && sLine.contains("]"))
-                    {
-                        int locEnd = sLine.indexOf("]");
-                        sSymbol = sLine.substring(1, locEnd);
-                        debug("--NEW SYM : " + sSymbol + "(" + line + ")");
-                        _htSymbols.put(sSymbol, line);
-                    }
-                    else
-                    {
-                        line++;
-                    }
-                }
-                sLine = br.readLine();
-            }
-        
-		} catch (IOException e) {
+			while (sLine != null)
+			{
+				// If line is not a comment
+				if (!sLine.startsWith("//"))
+				{
+					if (sLine.startsWith("[") && sLine.contains("]"))
+					{
+						final int locEnd = sLine.indexOf("]");
+						sSymbol = sLine.substring(1, locEnd);
+						debug("--NEW SYM : " + sSymbol + "(" + line + ")");
+						_htSymbols.put(sSymbol, line);
+					}
+					else
+					{
+						line++;
+					}
+				}
+				sLine = br.readLine();
+			}
+
+		} catch (final IOException e) {
 			debug("[getSymbols] IOException " + sSymbol + "(" + line + ")");
 			e.printStackTrace();
 		}
 
-    }
-    
-    protected void debug(final String sText)
-    {
-        if ( _bDebug )
-        {
-        	Log.d(TAG, sText);
-        }
-    }
+	}
+
+	protected void debug(final String sText)
+	{
+		if ( _bDebug )
+		{
+			Log.d(TAG, sText);
+		}
+	}
 }
