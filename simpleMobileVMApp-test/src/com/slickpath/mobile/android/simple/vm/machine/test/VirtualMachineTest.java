2184235355d0774a888da7c517a48ea6d6874279
ukonpete@gmail.com <ukonpete@gmail.com@661b4331-e04f-a31d-c30e-921faf56ea9a>
2012-02-22 09:55:01 +0000

    Final unit tests and bug fixes

--- simpleMobileVMApp-test/src/com/slickpath/mobile/android/simple/vm/machine/test/VirtualMachineTest.java
+++ simpleMobileVMApp-test/src/com/slickpath/mobile/android/simple/vm/machine/test/VirtualMachineTest.java
@@ -4,8 +4,10 @@
 package com.slickpath.mobile.android.simple.vm.machine.test;
 
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -14,20 +16,37 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import android.test.AndroidTestCase;
+import android.util.Log;
 
 import com.slickpath.mobile.android.simple.vm.IVMListener;
 import com.slickpath.mobile.android.simple.vm.VMError;
 import com.slickpath.mobile.android.simple.vm.instructions.Instructions;
 import com.slickpath.mobile.android.simple.vm.machine.VirtualMachine;
+import com.slickpath.mobile.android.simple.vm.parser.IParserListener;
+import com.slickpath.mobile.android.simple.vm.parser.SimpleParser;
 import com.slickpath.mobile.android.simple.vm.util.Command;
+import com.slickpath.mobile.android.simple.vm.util.CommandList;
 
 /**
  * @author PJ
  *
  */
-public class VirtualMachineTest extends AndroidTestCase implements IVMListener{
+public class VirtualMachineTest extends AndroidTestCase implements IVMListener, IParserListener{
+
+	private static final String TAG = VirtualMachineTest.class.getName();
+
+	private static final int NUM_COMMANDS_TO_RUN = 10;
 
 	private VirtualMachine _vm = null;
+
+	private SimpleParser _parser;
+	private CountDownLatch _signal;
+	private VMError _vmError;
+	private boolean _bHalt;
+	private int _lastLineExecuted;
+	private CommandList _commands;
+	private int _count ;
+
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -50,6 +69,10 @@ public class VirtualMachineTest extends AndroidTestCase implements IVMListener{
 	protected void setUp() throws Exception {
 		super.setUp();
 		_vm = new VirtualMachine(getContext());
+		_vm.setVMListener(this);
+		_bHalt = false;
+		_lastLineExecuted = -1;
+		_count = 0;
 	}
 
 	/* (non-Javadoc)
@@ -67,11 +90,11 @@ public class VirtualMachineTest extends AndroidTestCase implements IVMListener{
 	 */
 	@Test
 	public void testGetVMListener() {
-		assertNull(_vm.getVMListener());
-		_vm.setVMListener(this);
 		assertNotNull(_vm.getVMListener());
 		_vm.setVMListener(null);
 		assertNull(_vm.getVMListener());
+		_vm.setVMListener(this);
+		assertNotNull(_vm.getVMListener());
 	}
 
 	/**
@@ -79,7 +102,6 @@ public class VirtualMachineTest extends AndroidTestCase implements IVMListener{
 	 */
 	@Test
 	public void testAddCommand() {
-
 		final int [] instructions = {Instructions._ADD, Instructions._EQUAL, Instructions._NOT, Instructions._PUSHC, Instructions._JUMP, Instructions._POPC};
 		final Integer [] params = {null, null, null, 10, 20, 30};
 
@@ -104,7 +126,6 @@ public class VirtualMachineTest extends AndroidTestCase implements IVMListener{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 
 	/**
@@ -112,7 +133,43 @@ public class VirtualMachineTest extends AndroidTestCase implements IVMListener{
 	 */
 	@Test
 	public void testAddCommands() {
-		fail("Not yet implemented");
+		final int [] instructions = {Instructions._ADD, Instructions._EQUAL, Instructions._NOT, Instructions._PUSHC, Instructions._JUMP, Instructions._POPC};
+		final Integer [] params = {null, null, null, 10, 20, 30};
+
+		try {
+			final CommandList commandList = new CommandList();
+
+			for(int i = 0; i < instructions.length; i++)
+			{
+				final List<Integer> paramList = new ArrayList<Integer>();
+				final Integer param = params[i];
+				paramList.add(param);
+				final Command command = new Command(instructions[i],paramList);
+				commandList.add(command);
+			}
+			_signal = new CountDownLatch(1);
+			_vm.addCommands(commandList);
+			assertNull(_vmError);
+
+			try {
+				// Wait for Callback
+				_signal.await();
+			} catch (final InterruptedException e) {
+				e.printStackTrace();
+				fail(e.getMessage());
+			}// wait for callback
+
+			for(int i = 0; i < instructions.length; i++)
+			{
+				final Command command = _vm.getCommandAt(i);
+				assertEquals(instructions[i], command.getCommandId().intValue());
+				assertEquals(params[i], command.getParameters().get(0));
+			}
+
+		} catch (final VMError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -120,7 +177,70 @@ public class VirtualMachineTest extends AndroidTestCase implements IVMListener{
 	 */
 	@Test
 	public void testRunNextInstruction() {
-		fail("Not yet implemented");
+		int lineTracker = NUM_COMMANDS_TO_RUN;
+		final File filesDir = mContext.getFilesDir();
+		_parser = new SimpleParser(filesDir.getPath() + File.separator + "fibonacciAndroid.ins.txt", this);
+
+		_signal = new CountDownLatch(1);
+		Log.d(TAG, "+...........................PARSE START ");
+		_parser.parse();
+		try {
+			// Wait for Callback
+			Log.d(TAG, "+...........................PARSE WAIT ");
+			_signal.await();
+			Log.d(TAG, "+...........................PARSE DONE WAIT ");
+		} catch (final InterruptedException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}// wait for callback
+
+		_signal = new CountDownLatch(1);
+		Log.d(TAG, "+...........................VM ADD COMMANDS START");
+		_vm.addCommands(_commands);
+
+		try {
+			// Wait for Callback
+			Log.d(TAG, "+...........................VM ADD COMMANDS WAIT ");
+			_signal.await();
+			Log.d(TAG, "+...........................VM ADD COMMANDS DONE WAIT ");
+		} catch (final InterruptedException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}// wait for callback
+		assertNull(_vmError);
+
+		_signal = new CountDownLatch(1);
+		Log.d(TAG, "+...........................VM RUN INSTRS START");
+		_vm.runInstructions(NUM_COMMANDS_TO_RUN);
+
+		try {
+			// Wait for Callback
+			Log.d(TAG, "+...........................VM RUN INSTRS WAIT ");
+			_signal.await();
+			Log.d(TAG, "+...........................VM RUN INSTRS DONE WAIT ");
+		} catch (final InterruptedException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}// wait for callback
+
+		assertFalse(_bHalt);
+		assertEquals(lineTracker++, _lastLineExecuted);
+		assertNull(_vmError);
+
+		for(int i =0; i < 100; i++)
+		{
+			_signal = new CountDownLatch(1);
+			_vm.runNextInstruction();
+			try {
+				// Wait for Callback
+				_signal.await();
+			} catch (final InterruptedException e) {
+				e.printStackTrace();
+				fail(e.getMessage());
+			}// wait for callback
+			assertFalse("("+_count+")LINE=" + _lastLineExecuted, _bHalt);
+			assertNull("("+_count+")LINE=" + _lastLineExecuted, _vmError);
+		}
 	}
 
 	/**
@@ -128,7 +248,36 @@ public class VirtualMachineTest extends AndroidTestCase implements IVMListener{
 	 */
 	@Test
 	public void testRunInstructions() {
-		fail("Not yet implemented");
+		final File filesDir = mContext.getFilesDir();
+		_parser = new SimpleParser(filesDir.getPath() + File.separator + "fibonacciAndroid.ins.txt", this);
+		_signal = new CountDownLatch(1);
+
+		_parser.parse();
+		try {
+			// Wait for Callback
+			_signal.await();
+		} catch (final InterruptedException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}// wait for callback
+
+		_vm.addCommands(_commands);
+		assertNull(_vmError);
+
+		_signal = new CountDownLatch(1);
+
+		_vm.runInstructions();
+		try {
+			// Wait for Callback
+			_signal.await();
+		} catch (final InterruptedException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}// wait for callback
+
+		assertFalse(_bHalt);
+		assertEquals(-1, _lastLineExecuted);
+		assertNull(_vmError);
 	}
 
 	/**
@@ -136,20 +285,67 @@ public class VirtualMachineTest extends AndroidTestCase implements IVMListener{
 	 */
 	@Test
 	public void testRunInstructionsInt() {
-		fail("Not yet implemented");
+		final File filesDir = mContext.getFilesDir();
+		_parser = new SimpleParser(filesDir.getPath() + File.separator + "fibonacciAndroid.ins.txt", this);
+		_signal = new CountDownLatch(1);
+
+		_parser.parse();
+		try {
+			// Wait for Callback
+			_signal.await();
+		} catch (final InterruptedException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}// wait for callback
+
+		_vm.addCommands(_commands);
+		assertNull(_vmError);
+
+		_signal = new CountDownLatch(1);
+		_vm.runInstructions(10);
+
+		try {
+			// Wait for Callback
+			_signal.await();
+		} catch (final InterruptedException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}// wait for callback
+
+		assertFalse(_bHalt);
+		assertEquals(10, _lastLineExecuted);
+		assertNull(_vmError);
 	}
 
 	@Override
 	public void completedAddingInstructions(final VMError vmError) {
 		// TODO Auto-generated method stub
-
+		Log.d(TAG, "+...........................completedAddingInstructions ");
+		_vmError = vmError;
+		_signal.countDown();// notify the count down latch
+		Log.d(TAG, "+...........................completedAddingInstructions CountDown");
 	}
 
 	@Override
 	public void completedRunningInstructions(final boolean bHalt,
 			final int lastLineExecuted, final VMError vmError) {
 		// TODO Auto-generated method stub
-
+		Log.d(TAG, "+...........................CompletedRunningInstructions " + lastLineExecuted);
+		_vmError = vmError;
+		_bHalt = bHalt;
+		_lastLineExecuted = lastLineExecuted;
+		_count++;
+		_signal.countDown();// notify the count down latch
+		Log.d(TAG, "+...........................CompletedRunningInstructions CountDown");
 	}
 
+	@Override
+	public void completedParse(final VMError vmError, final CommandList commands) {
+		// Save values on callback and release test thread
+		Log.d(TAG, "+...........................completedParse ");
+		_vmError = vmError;
+		_commands = commands;
+		_signal.countDown();// notify the count down latch
+		Log.d(TAG, "+...........................completedParse CountDown");
+	}
 }
