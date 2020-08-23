965e85f77c00d639283d6998240fdec6163e2dc4
Pete P <pete.procopio@epicgames.com>
2020-08-23 14:41:48 -0400

    Remove Butterknife

--- simpleMobileVMApp/app/src/main/java/com/slickpath/mobile/android/simple/vm/app/SimpleMobileVMAppActivity.java
+++ simpleMobileVMApp/app/src/main/java/com/slickpath/mobile/android/simple/vm/app/SimpleMobileVMAppActivity.java
@@ -3,8 +3,12 @@ package com.slickpath.mobile.android.simple.vm.app;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -20,69 +24,68 @@ import com.slickpath.mobile.android.simple.vm.util.CommandList;
 
 import java.io.IOException;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.OnItemSelected;
-
+@SuppressWarnings("FieldCanBeLocal")
 public class SimpleMobileVMAppActivity extends Activity implements IVMListener, IParserListener {
 
+    @NonNull
     private final StringBuilder stringBuilder = new StringBuilder();
     // TODO ProgressDialog has been deprecated
     private ProgressDialog progressDialog;
-
     private VirtualMachine _vm;
 
-    @SuppressWarnings("WeakerAccess")
-    @BindView(R.id.textViewPath)
-    TextView textViewPath;
-
-    @SuppressWarnings("WeakerAccess")
-    @BindView(R.id.spinnerFiles)
-    Spinner spinnerFiles;
-
-    @SuppressWarnings("WeakerAccess")
-    @BindView(R.id.editTextOutput)
-    EditText editTextOutput;
-
-    @SuppressWarnings("WeakerAccess")
-    @BindView(R.id.textViewFile)
-    TextView textViewFile;
+    private TextView textViewPath;
+    private Spinner spinnerFiles;
+    private EditText editTextOutput;
+    private TextView textViewFile;
+    private Button buttonExe;
 
     @Override
     public void onCreate(final Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        ButterKnife.bind(this);
+        textViewPath = findViewById(R.id.textViewPath);
+        spinnerFiles = findViewById(R.id.spinnerFiles);
+        editTextOutput = findViewById(R.id.editTextOutput);
+        textViewFile = findViewById(R.id.textViewFile);
+        buttonExe = findViewById(R.id.buttonExe);
 
         textViewPath.setText(getInstructionPath());
 
         final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, getInstructionFiles());
         spinnerFiles.setAdapter(adapter);
-    }
 
-    @OnItemSelected(R.id.spinnerFiles)
-    public void spinnerFilesItemSelected(int position) {
-        getSelectedFileName(position);
-    }
-
-    @OnClick(R.id.buttonExe)
-    public void onClickExeButton() {
-        editTextOutput.setText("");
-        _vm = new VirtualMachine(SimpleMobileVMAppActivity.this.getApplicationContext(), new OutputListener() {
+        buttonExe.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void charOutput(char c) {
-                stringBuilder.append(c);
+            public void onClick(View v) {
+                editTextOutput.setText("");
+                _vm = new VirtualMachine(SimpleMobileVMAppActivity.this.getApplicationContext(), new OutputListener() {
+                    @Override
+                    public void charOutput(char c) {
+                        stringBuilder.append(c);
+                    }
+
+                    @Override
+                    public void lineOutput(String line) {
+                        stringBuilder.append(line);
+                    }
+                }, null);
+                _vm.setVMListener(SimpleMobileVMAppActivity.this);
+                parseFile((int) spinnerFiles.getSelectedItemId());
+            }
+        });
+
+        spinnerFiles.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                getSelectedFileName(position);
             }
 
             @Override
-            public void lineOutput(String line) {
-                stringBuilder.append(line);
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO
             }
-        }, null);
-        _vm.setVMListener(SimpleMobileVMAppActivity.this);
-        parseFile((int) spinnerFiles.getSelectedItemId());
+        });
     }
 
     /**
