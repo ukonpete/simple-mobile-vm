4ebc1f5e2a1696b9f164458402f7cb6c3723f39e
Pete P <pete.procopio@epicgames.com>
2020-08-23 22:36:39 -0400

    1st pass view models

---
+++ simpleMobileVMApp/app/src/main/java/com/slickpath/mobile/android/simple/vm/app/viewmodel/SimpleVMViewModel.kt
@@ -0,0 +1,52 @@
+package com.slickpath.mobile.android.simple.vm.app.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.slickpath.mobile.android.simple.vm.IVMListener
+import com.slickpath.mobile.android.simple.vm.VMError
+import com.slickpath.mobile.android.simple.vm.app.SimpleMobileVMAppActivity
+import com.slickpath.mobile.android.simple.vm.machine.VirtualMachine
+import com.slickpath.mobile.android.simple.vm.util.CommandList
+
+class SimpleVMViewModel(private var virtualMachine: VirtualMachine) : ViewModel(), IVMListener {
+
+
+    init {
+        virtualMachine.vMListener = this
+    }
+
+    val onCompletedAddingInstructions: MutableLiveData<CompletedAddingInstructionsStatus> by lazy {
+        MutableLiveData<CompletedAddingInstructionsStatus>()
+    }
+
+    val onCompletedRunningInstructionsInstructionsStatus: MutableLiveData<CompletedRunningInstructionsStatus> by lazy {
+        MutableLiveData<CompletedRunningInstructionsStatus>()
+    }
+
+    fun addCommands(commands: CommandList?) {
+        virtualMachine.reset()
+        virtualMachine.addCommands(commands)
+    }
+
+    fun runInstructions() {
+        virtualMachine.runInstructions()
+    }
+
+    fun reset(context: Context, outputListener : SimpleMobileVMAppActivity.SimpleVMOutputListener) {
+        virtualMachine = VirtualMachine(context, outputListener, null)
+        virtualMachine.vMListener = this
+    }
+
+    override fun completedAddingInstructions(vmError: VMError?) {
+        onCompletedAddingInstructions.postValue(CompletedAddingInstructionsStatus(vmError))
+    }
+
+    override fun completedRunningInstructions(bHalt: Boolean, lastLineExecuted: Int, vmError: VMError?) {
+        onCompletedRunningInstructionsInstructionsStatus.postValue(CompletedRunningInstructionsStatus(bHalt, lastLineExecuted, vmError))
+    }
+}
+
+data class CompletedRunningInstructionsStatus(val onHalt: Boolean, val lastLineExecuted: Int, val vmError: VMError?)
+
+data class CompletedAddingInstructionsStatus(val vmError: VMError?)
