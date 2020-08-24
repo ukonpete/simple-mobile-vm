8e0011f1a37bc43287ded0a53576fbbb8471e9c2
Pete P <pete.procopio@epicgames.com>
2020-08-23 21:15:57 -0400

    Change main view to ConstraintLayout

--- simpleMobileVMApp/app/src/main/java/com/slickpath/mobile/android/simple/vm/app/SimpleMobileVMAppActivity.kt
+++ simpleMobileVMApp/app/src/main/java/com/slickpath/mobile/android/simple/vm/app/SimpleMobileVMAppActivity.kt
@@ -1,9 +1,11 @@
 package com.slickpath.mobile.android.simple.vm.app
 
 import android.app.Activity
-import android.app.ProgressDialog
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.widget.*
 import android.widget.AdapterView.OnItemSelectedListener
 import com.slickpath.mobile.android.simple.vm.IVMListener
@@ -15,65 +17,49 @@ import com.slickpath.mobile.android.simple.vm.parser.SimpleParser
 import com.slickpath.mobile.android.simple.vm.util.CommandList
 import java.io.IOException
 
+
 class SimpleMobileVMAppActivity : Activity(), IVMListener, IParserListener {
     private val stringBuilder = StringBuilder()
 
-    // TODO ProgressDialog has been deprecated
-    private var progressDialog: ProgressDialog? = null
-
-    private lateinit var _vm: VirtualMachine
-    private lateinit var textViewPath: TextView
+    private lateinit var virtualMachine: VirtualMachine
     private lateinit var spinnerFiles: Spinner
-    private lateinit var editTextOutput: EditText
+    private lateinit var editTextOutput: TextView
     private lateinit var textViewFile: TextView
     private lateinit var buttonExe: Button
+    private lateinit var progressBar: ProgressBar
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
-        textViewPath = findViewById(R.id.textViewPath)
+
         spinnerFiles = findViewById(R.id.spinnerFiles)
         editTextOutput = findViewById(R.id.editTextOutput)
         textViewFile = findViewById(R.id.textViewFile)
         buttonExe = findViewById(R.id.buttonExe)
-        textViewPath.text = instructionPath
+        progressBar = findViewById(R.id.progressBar)
+
+        editTextOutput.movementMethod = ScrollingMovementMethod()
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, instructionFiles)
         spinnerFiles.adapter = adapter
-        buttonExe.setOnClickListener {
-            editTextOutput.setText("")
-            _vm = VirtualMachine(this@SimpleMobileVMAppActivity.applicationContext, object : OutputListener {
-                override fun charOutput(c: Char) {
-                    stringBuilder.append(c)
-                }
-
-                override fun lineOutput(line: String) {
-                    stringBuilder.append(line)
-                }
-            }, null)
-            _vm.vMListener = this@SimpleMobileVMAppActivity
-            parseFile(spinnerFiles.selectedItemId.toInt())
-        }
-        spinnerFiles.onItemSelectedListener = object : OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
-                getSelectedFileName(position)
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                // DO NOTHING
-            }
-        }
+        buttonExe.setOnClickListener(ExecuteButtonOnClickListener())
+        spinnerFiles.onItemSelectedListener = FileItemSelectedListener()
     }
 
-    /**
-     * @param index index into file list
-     */
-    private fun parseFile(index: Int) {
-        val selectedFile = getSelectedFileName(index)
+    private fun onExecuteClicked() {
+        editTextOutput.text = ""
+        editTextOutput.scrollTo(0, 0)
+
+        progressBar.visibility = VISIBLE
+        virtualMachine = VirtualMachine(applicationContext, SimpleVMOutputListener(), null)
+        virtualMachine.vMListener = this
+        parseFile(spinnerFiles.selectedItemId.toInt())
+    }
+
+    private fun parseFile(fileListIndex: Int) {
+        val selectedFile = getSelectedFileName(fileListIndex)
         try {
             val simpleMobileVMFileHelper = SimpleMobileVMFileHelper(applicationContext, instructionPath, selectedFile)
             val parser = SimpleParser(simpleMobileVMFileHelper, this)
-            progressDialog = ProgressDialog.show(this@SimpleMobileVMAppActivity, "",
-                    "Please wait for few seconds...", true)
             parser.parse()
         } catch (e: IOException) {
             Toast.makeText(this, "Unable to find file $selectedFile", Toast.LENGTH_LONG).show()
@@ -116,7 +102,7 @@ class SimpleMobileVMAppActivity : Activity(), IVMListener, IParserListener {
             Toast.makeText(this, "ERROR PARSE" + vmError.message, Toast.LENGTH_LONG).show()
             vmError.printStackTrace()
         }
-        _vm.addCommands(commands)
+        virtualMachine.addCommands(commands)
     }
 
     override fun completedAddingInstructions(vmError: VMError?) {
@@ -124,19 +110,48 @@ class SimpleMobileVMAppActivity : Activity(), IVMListener, IParserListener {
             Toast.makeText(this, "ERROR ADD INST" + vmError.message, Toast.LENGTH_LONG).show()
             vmError.printStackTrace()
         }
-        _vm.runInstructions()
+        virtualMachine.runInstructions()
     }
 
     override fun completedRunningInstructions(bHalt: Boolean, lastLineExecuted: Int, vmError: VMError?) {
-        progressDialog!!.dismiss()
+        runOnUiThread {
+            progressBar.visibility = GONE
+        }
         if (vmError != null) {
             Toast.makeText(this, "ERROR RUN INST lastLine=$lastLineExecuted", Toast.LENGTH_LONG).show()
             vmError.printStackTrace()
         } else {
             runOnUiThread {
-                editTextOutput.setText(stringBuilder.toString())
+                editTextOutput.text = stringBuilder.toString()
                 stringBuilder.setLength(0)
             }
+        }
+    }
+
+
+    inner class ExecuteButtonOnClickListener : View.OnClickListener {
+        override fun onClick(v: View?) {
+            onExecuteClicked()
+        }
+    }
+
+    inner class SimpleVMOutputListener : OutputListener {
+        override fun charOutput(c: Char) {
+            stringBuilder.append(c)
+        }
+
+        override fun lineOutput(line: String) {
+            stringBuilder.append(line)
+        }
+    }
+
+    inner class FileItemSelectedListener : OnItemSelectedListener {
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            getSelectedFileName(position)
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+            // DO NOTHING
         }
     }
 }
